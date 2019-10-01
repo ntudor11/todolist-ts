04411ddd7bf5f03134e5ca8876a686ddbaecf59a
Tudor Nastasa <cwb382@alumni.ku.dk>
2019-10-01 12:37:48 +0200

    variables within html code

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var container = document.getElementById('container');
-function countdown(initial, final, interval) {
-    if (final === void 0) { final = 0; }
-    var current = initial;
-    while (current > final) {
-        container.innerHTML = current;
-        current -= interval;
-    }
-}
-countdown(10, 0, 1);
+var todo = {
+    id: 123,
+    name: "pick up drycleaning",
+    completed: true
+};
+container.innerHTML = "\n<div id='" + todo.id + "' class=\"list-group-item}\">\n    <i class=\"" + (todo.completed ? "" : "hidden") + " text-success glyphicon glyphicon-ok\"></i>\n    <span class=\"name\">" + todo.name + "</span>\n</div>\n";
+// function countdown(initial, final = 0, interval) {
+//
+//   var current = initial;
+//
+//   while (current > final) {
+//     container.innerHTML = current;
+//     current -= interval;
+//   }
+// }
+//
+// countdown(10, 0, 1)
--- app.ts
+++ app.ts
@@ -1,13 +1,26 @@
 var container = document.getElementById('container');
 
-function countdown(initial, final = 0, interval) {
-
-  var current = initial;
-
-  while (current > final) {
-    container.innerHTML = current;
-    current -= interval;
-  }
+var todo = {
+  id: 123,
+  name: "pick up drycleaning",
+  completed: true
 }
 
-countdown(10, 0, 1)
+container.innerHTML = `
+<div id='${todo.id}' class="list-group-item}">
+    <i class="${todo.completed ? "" : "hidden" } text-success glyphicon glyphicon-ok"></i>
+    <span class="name">${todo.name}</span>
+</div>
+`
+
+// function countdown(initial, final = 0, interval) {
+//
+//   var current = initial;
+//
+//   while (current > final) {
+//     container.innerHTML = current;
+//     current -= interval;
+//   }
+// }
+//
+// countdown(10, 0, 1)
